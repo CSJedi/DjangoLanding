553bd37923f545b4f3411c0a844a3c053af1f447
tetiana.zhydeikina <tetiana.zhydeikina.kn.2016@lpnu.ua>
2018-01-13 19:51:25 +0200

    add calculator.js

---
+++ landing/static/scripts/calculator.js
@@ -0,0 +1,5 @@
+ function calculator(){
+                document.getElementById("result").value = 
+                ((parseInt(document.getElementById("kg").value) 
+                	+ parseInt(document.getElementById("volume").value))/2) + "$";
+            }
